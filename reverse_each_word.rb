f5d4e39c74af441088b70f486ee01552664872a6
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-15 06:13:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   split_sentence1 = sentence1.split("")
   reversed = []
-  sentence1.size.times { reversed << split_sentence1.pop } 
+  sentence1.times { reversed << split_sentence1.pop } 
   reversed.join 
 end
 puts reverse_each_word("Hello there, and how are you?")
