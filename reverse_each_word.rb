db35143a41a7d77acb06e2ebd08b9ad9fc287d52
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-15 06:32:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,3 @@
-# def reverse_each_word(sentence1)
-#   split_sentence1 = sentence1.split("")
-#   reversed = []
-#   sentence1.size.times { reversed << split_sentence1.pop } 
-#   reversed.join
-# end
-
 def reverse_each_word(sentence1)
   split_sentence1 = sentence1.split(" ")
   reversed = []
